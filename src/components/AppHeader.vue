d796b0629fbea14020a49e37da58bcd87c0880e7
iTsGiuse <giuse.amato@icloud.com>
2024-04-15 16:09:07 +0200

    new exercise

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+    
+    export default {
+        name: 'AppHeader'
+    }
+
+</script>
+
+<template>
+
+    <div class="container">
+        <h1>Yu-Gi-Oh Api</h1>
+    </div>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
